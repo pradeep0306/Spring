ad7ffb3353a0342d9be2a105540824a859330cf9
pradeeppenumatcha03@gmail.com <pradeeppenumatcha03@gmail.com>
2017-01-28 22:19:44 +0530

    Retriving Users Rest Webservice

--- src/main/java/com/projectTest/Controller/UsersController.java
+++ src/main/java/com/projectTest/Controller/UsersController.java
@@ -15,9 +15,15 @@ public class UsersController {
 	{
 		
 		RestTemplate restTemplate = new RestTemplate();
-		String url = "";
-		Users user = restTemplate.getForObject(url, Users.class);
-		return new ModelAndView("usersPage","result",user.getName());
+		String url = "https://jsonplaceholder.typicode.com/users";
+		Users[] user = restTemplate.getForObject(url, Users[].class);
+		//String user=restTemplate.getForObject(url, String.class);
+		//for(Users record:user)
+		//{
+		//	record.getName();
+		//}
+		
+		return new ModelAndView("usersPage","result",user[2].getEmail());
 	}
 	
 }
